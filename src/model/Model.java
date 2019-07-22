11c2ea0e3f4226124da5b94dedbca817108adae9
arhidalgo <arhidalgo@arhidalgoVirtualBox>
2019-07-22 13:22:41 +0200

    Estado inicial

---
+++ src/model/Model.java
@@ -0,0 +1,20 @@
+package model;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class Model
+ */
+@Stateless
+@LocalBean
+public class Model {
+
+    /**
+     * Default constructor. 
+     */
+    public Model() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
